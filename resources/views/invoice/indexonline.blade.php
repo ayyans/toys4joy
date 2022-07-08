048c2d453c1007a31af6b10802073503ad076b42
Ahsin Javaid <ahsinjavaid890@gmail.com>
2022-07-08 16:06:03 +0500

    new changes

--- resources/views/invoice/indexonline.blade.php
+++ resources/views/invoice/indexonline.blade.php
@@ -138,7 +138,7 @@
                   Order ID #: {{ $ordernumber }}<br />
                   Order Date: {{ date('M d, Y', strtotime($order->first()->created_at)) }}<br />
                   Invoice Status: <b style="color: green;">
-                    @if($order->first()->mode == 1)
+                    @if($order->first()->mode == 2)
                   PAID
                   @else 
 
