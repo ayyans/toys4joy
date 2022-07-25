fb91b6ce104934155a0aa5aaadc41090f69e9cc0
ayyans <ayman.yed123@gmail.com>
2022-07-25 14:41:19 +0300

    Update home.blade.php

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -23,16 +23,19 @@
                     @include('website.layouts.user_menu')
                     <div id="carouselExampleIndicators_mobile" class="carousel slide" data-bs-ride="carousel">
                       <div class="carousel-indicators">
-                      {{$i=0}}
+                      @php
+                     $i = 0
+                     @endphp
                       @foreach(DB::table('homepagebanners')->where('status' , 2)->get() as $r)
-                      @if ($i!==0)
-                      <button type="button" data-bs-target="#carouselExampleIndicators_mobile" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                      @if ($i==0)
+                      <button data-bs-target="#carouselExampleIndicators_mobile" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 0"></button>
                       @else
-                      <button type="button" data-bs-target="#carouselExampleIndicators_mobile" data-bs-slide-to="{{$i}}" aria-label="Slide 2"></button>
+                      <button data-bs-target="#carouselExampleIndicators_mobile" data-bs-slide-to="{{$i}}" aria-label="Slide {{$i}}"></button>
                       @endif 
-                      {{$i+1}}
+                      @php 
+                      $i++;
+                      @endphp
                         @endforeach
-                        
                       </div>
                       <div class="carousel-inner">
                         @foreach(DB::table('homepagebanners')->where('status' , 2)->get() as $r)
@@ -69,14 +72,18 @@
                 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
                 
                   <div class="carousel-indicators">
-                      
+                      @php
+                     $i = 0;
+                     @endphp
                       @foreach(DB::table('homepagebanners')->where('status' , 2)->get() as $r)
-                      @if ($i!==0)
-                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                      @if ($i==0)
+                      <button data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                       @else
-                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="{{$i++}}" aria-label="Slide 2"></button>
+                      <button data-bs-target="#carouselExampleIndicators" data-bs-slide-to="{{$i}}" aria-label="Slide {{$i}}"></button>
                       @endif 
-                     
+                      @php 
+                      $i++;
+                      @endphp
                         @endforeach
                         
                 </div>
@@ -172,16 +179,8 @@
 <div class="col-sm-2 col-md-2 col-xl-2">
 <div class="row">
 <div class="owl-carousel-single-picture owl-carousel owl-theme mt-3">
-@foreach($products as $product)
-  <div class="item single"><img class="img-widget" src="{{asset('products/'.$product->featured_img)}}"></div>
-@endforeach 
-</div>
-</div>
-<div class="row">
-<div class="owl-carousel-single-picture owl-carousel owl-theme mt-3">
-@foreach($products as $product)
-  <div class="item single"><img class="img-widget" src="{{asset('products/'.$product->featured_img)}}"></div>
-@endforeach 
+<div class="item single"><img class="img-widget" src="{{asset('uploads/ad-1.png')}}"></div>
+<div class="item single"><img class="img-widget" src="{{asset('uploads/ad-2.png')}}"></div>
 </div>
 </div>
 </div>
