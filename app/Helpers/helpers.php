39bfd9b1b7c1aade902087ac0869bd49dbcd8f56
TalhaMoazSarwar <iamtms@hotmail.com>
2022-08-09 18:14:24 +0500

    sadad methods to helper

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,7 +1,49 @@
 <?php
 
-if (! function_exists('cart') ) {
-  function cart() {
+// sadad functions
+
+if (!function_exists('getChecksumFromString')) {
+  function getChecksumFromString($str, $key)
+  {
+    $salt = generateSalt_e(4);
+    $finalString = $str . "|" . $salt;
+    $hash = hash("sha256", $finalString);
+    $hashString = $hash . $salt;
+    $checksum = encrypt_e($hashString, $key);
+    return $checksum;
+  }
+}
+
+if (!function_exists('generateSalt_e')) {
+  function generateSalt_e($length)
+  {
+    $random = "";
+    srand((float) microtime() * 1000000);
+    $data = "AbcDE123IJKLMN67QRSTUVWXYZ";
+    $data .= "aBCdefghijklmn123opq45rs67tuv89wxyz";
+    $data .= "0FGH45OP89";
+    for ($i = 0; $i < $length; $i++) {
+      $random .= substr($data, (rand() % (strlen($data))), 1);
+    }
+    return $random;
+  }
+}
+
+if (!function_exists('encrypt_e')) {
+  function encrypt_e($input, $ky)
+  {
+    $ky = html_entity_decode($ky);
+    $iv = "@@@@&&&&####$$$$";
+    $data = openssl_encrypt($input, "AES-128-CBC", $ky, 0, $iv);
+    return $data;
+  }
+}
+
+// general functions
+
+if (!function_exists('cart')) {
+  function cart()
+  {
     $session = auth()->check() ? auth()->id() : session()->getId();
     return app('cart')->session($session);
   }
--- resources/views/website/payasmember.blade.php
+++ resources/views/website/payasmember.blade.php
@@ -122,35 +122,7 @@
 </div>
 
 </main>
-<?php 
-function getChecksumFromString($str, $key) { 
-
- $salt = generateSalt_e(4); 
- $finalString = $str . "|" . $salt; 
- $hash = hash("sha256", $finalString); 
- $hashString = $hash . $salt; 
- $checksum = encrypt_e($hashString, $key); 
- return $checksum; 
-
-} 
-
-function generateSalt_e($length) { 
-
- $random = ""; 
- srand((double) microtime() * 1000000); 
- $data = "AbcDE123IJKLMN67QRSTUVWXYZ"; 
- $data .= "aBCdefghijklmn123opq45rs67tuv89wxyz"; 
- $data .= "0FGH45OP89"; 
- for ($i = 0; $i < $length; $i++) { 
- $random .= substr($data, (rand() % (strlen($data))), 1);  } 
- return $random; 
-} 
-
-function encrypt_e($input, $ky) { 
- $ky = html_entity_decode($ky); 
- $iv = "@@@@&&&&####$$$$"; 
- $data = openssl_encrypt($input, "AES-128-CBC", $ky, 0, $iv);  return $data; 
-} 
+<?php
 
  $sadad_checksum_array = array(); 
  $sadad__checksum_data = array(); 
@@ -174,7 +146,7 @@ if(Auth::check())
  $sadad_checksum_array['EMAIL'] = $email; 
  $sadad_checksum_array['MOBILE_NO'] = '999999999';  
  $sadad_checksum_array['SADAD_WEBCHECKOUT_PAGE_LANGUAGE'] = 'ENG';  
- $sadad_checksum_array['CALLBACK_URL'] = url('orderconferm'); 
+ $sadad_checksum_array['CALLBACK_URL'] = url('orderconferm');
  $sadad_checksum_array['txnDate'] = $txnDate; 
 
 foreach ($items as $item) {
