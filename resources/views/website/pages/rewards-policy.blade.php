c3f2d97bbc25f43b7ceb51613021fc0902ff1038
ayyans <ayman.yed123@gmail.com>
2022-08-14 18:11:36 +0300

    Update rewards-policy.blade.php

--- resources/views/website/pages/rewards-policy.blade.php
+++ resources/views/website/pages/rewards-policy.blade.php
@@ -6,12 +6,10 @@
         <div class="col-3 categories-col">
             <div class="d-flex flex-column flex-shrink-0" >
                 <div class="for-mobile mbl-banner">
-                @include('website.layouts.user_menu') 
-                    <img src="{{asset('website/img/t1.png')}}" class="img-fluid">
+                @include('website.layouts.user_menu')    
                 </div>
                 <div class="categories">
                     <h2 class="for-desktop">Categories</h2>
-                    
                     <ul class="nav nav-pills flex-column mb-auto menu">
                      @include('website.layouts.category_menu')
                       
