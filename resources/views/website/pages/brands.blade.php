0dc0c7b55bf47956951f9dc9f362e2d9882b03ad
ayyans <ayman.yed123@gmail.com>
2022-08-14 17:50:15 +0300

    Update brands.blade.php

--- resources/views/website/pages/brands.blade.php
+++ resources/views/website/pages/brands.blade.php
@@ -6,12 +6,11 @@
     	<div class="col-3 categories-col">
             <div class="d-flex flex-column flex-shrink-0" >
                 <div class="for-mobile mbl-banner">
-                    @include('website.layouts.user_menu') 
+                @include('website.layouts.user_menu') 
                     <!--<img src="{{asset('website/img/t1.png')}}" class="img-fluid">-->
                 </div>
                 <div class="categories">
                     <h2 class="for-desktop">Categories</h2>
-                    <h2 class="for-mobile">Select Category</h2>
                     <ul class="nav nav-pills flex-column mb-auto menu">
                      @include('website.layouts.category_menu')
                       
