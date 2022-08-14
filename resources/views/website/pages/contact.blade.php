8ad939987c8cb0b2676bfe73f26dcd461e6f27ec
ayyans <ayman.yed123@gmail.com>
2022-08-14 13:30:58 +0300

    Update contact.blade.php

--- resources/views/website/pages/contact.blade.php
+++ resources/views/website/pages/contact.blade.php
@@ -7,25 +7,11 @@
     	<div class="col-3 categories-col">
             <div class="d-flex flex-column flex-shrink-0" >
                 <div class="for-mobile mbl-banner">
-                    <ul class="nav nav-pills nav-fill">
-                      <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">Best Offers</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Wish List</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">E-Gift Cards</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link">Your Points</a>
-                      </li>
-                    </ul>    
+                @include('website.layouts.user_menu') 
                     <img src="{{asset('website/img/t1.png')}}" class="img-fluid">
                 </div>
                 <div class="categories">
                     <h2 class="for-desktop">Categories</h2>
-                    <h2 class="for-mobile">Select Category</h2>
                     <ul class="nav nav-pills flex-column mb-auto menu">
                      @include('website.layouts.category_menu')
                       
@@ -50,11 +36,13 @@
                                 <span>operation@toys4joy.com</span>
                                 <span>info@toys4joy.com</span>
                             </li>
-                            <li><strong>Address:</strong> Toys 4 joy, 7GJ3+766, Doha, Qatar</li>
+                            <li><strong>coordinations:</strong> Toys 4 joy, 7GJ3+766, Doha, Qatar</li>
+                            <li><strong>Address:</strong> Building 25, zone 39, street 343, 4th floor, office 31, P.O 13920, Doha</li>
+                            
                         </ul>
                     </div>
-                <div class="col-md-4 col-sm-12 content-block map">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d901.9250610719338!2d51.5024664!3d25.2806672!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e45db773e60e063%3A0xf23502d4ac817c93!2sToys%204%20joy!5e0!3m2!1sen!2s!4v1655554050638!5m2!1sen!2s" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <div class="col-md-4 col-sm-12 content-block map"> 
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3607.70028342721!2d51.50079251496338!3d25.28066588385736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e45db08d1dc918d%3A0xabe71fea30cd8b5b!2sToys%204%20Joy!5e0!3m2!1sen!2sqa!4v1660379606706!5m2!1sen!2sqa" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>   
                 </div>
             </div>
         </div>
