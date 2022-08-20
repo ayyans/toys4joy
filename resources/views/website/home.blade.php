c9cb57952b6577890f80ce578bcebde40e50e1b3
TalhaMoazSarwar <iamtms@hotmail.com>
2022-08-20 17:18:32 +0500

    featured changed to back to school

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -197,7 +197,7 @@
 <!--section-->
 
 <!--section-->
-<div class="section-title"><h3>Featured</h3></div>
+<div class="section-title"><h3>Back to School</h3></div>
 <div class="row">
 <div class="owl-carousel-features owl-carousel owl-theme">
 @foreach($newarrivals as $product)
