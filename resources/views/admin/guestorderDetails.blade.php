5a66bc7fbf6766a388fe133838a5b5e87b7fa1d9
TalhaMoazSarwar <iamtms@hotmail.com>
2022-08-21 11:52:09 +0500

    order address for guest added in invoice

--- resources/views/admin/guestorderDetails.blade.php
+++ resources/views/admin/guestorderDetails.blade.php
@@ -51,7 +51,7 @@
 
                 </tr>
                 <tr>
-                    <td colspan="1">
+                    <td colspan="1" style="max-width: 250px">
                         <strong>{{ $order->additional_details['name'] ?? null }}</strong><br />
                         <strong> {{ $order->additional_details['email'] ?? null }}</strong><br />
                         <strong>{{ $order->additional_details['mobile'] ?? null }}</strong><br />
