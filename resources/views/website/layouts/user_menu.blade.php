2024fd363eb5684d3b6369177ffa8f6c5c870a98
mohamed ayman yeddes <ayman.yed123@gmail.com>
2022-09-01 09:54:05 +0300

    Update user_menu.blade.php

--- resources/views/website/layouts/user_menu.blade.php
+++ resources/views/website/layouts/user_menu.blade.php
@@ -1,48 +1,15 @@
-<ul class="nav nav-pills nav-fill treat-wrapper">
+<ul class="nav nav-pills nav-fill">
   <li class="nav-item">
     <?php //{{ route('website.newarrivals') }} ?>
-    <!--<a class="nav-link active" aria-current="page" href="{{ route('website.bestoffers') }}">Special Prices</a>-->
-   
-    <!--<a href="{{ url('contact-us') }}" class="pushable pushable-contact">
-          <span class="shadow"></span>
-          <span class="edge"></span>
-           <span class="front front-menu">
-           Special Prices
-           </span>
-</a>--> 
-<a href="{{ route('website.bestoffers') }}" class="btn-red treat-button">Special prices</a>
+    <a class="nav-link active" aria-current="page" href="{{ route('website.bestoffers') }}">Special Prices</a>
   </li>
   <li class="nav-item">
-    <!--<a class="nav-link" href="@if(Auth::check())  {{route('website.mywishlist',[encrypt(Auth::user()->id)])}} @else {{ url('login') }} @endif">Wish List</a>-->
-    <!--<a href="@if(Auth::check())  {{route('website.mywishlist',[encrypt(Auth::user()->id)])}} @else {{ url('login') }} @endif" class="pushable pushable-contact">
-          <span class="shadow"></span>
-          <span class="edge"></span>
-           <span class="front front-menu">
-          Wish list
-           </span>
-</a>--> 
-<a href="@if(Auth::check())  {{route('website.mywishlist',[encrypt(Auth::user()->id)])}} @else {{ url('login') }} @endif" class="btn-green treat-button">Wish list</a>
+    <a class="nav-link" href="@if(Auth::check())  {{route('website.mywishlist',[encrypt(Auth::user()->id)])}} @else {{ url('login') }} @endif">Wish List</a>
   </li>
   <li class="nav-item">
-    <!--<a class="nav-link" href="{{route('website.giftcard')}}">E-Gift Cards</a>-->
-    <!--<a href="@if(Auth::check())  {{route('website.mywishlist',[encrypt(Auth::user()->id)])}} @else {{ url('login') }} @endif" class="pushable pushable-contact">
-          <span class="shadow"></span>
-          <span class="edge"></span>
-           <span class="front front-menu">
-           E-Gift Cards
-           </span>
-</a>--> 
-<a href="{{route('website.giftcard')}}" class="btn-yellow treat-button">E-Gifts Card</a>
+    <a class="nav-link" href="{{route('website.giftcard')}}">E-Gift Cards</a>
   </li>
   <li class="nav-item">
-    <!--<a class="nav-link" href="{{route('website.yourpoints')}}">Your Points</a>-->
-    <!--<a href="@if(Auth::check())  {{route('website.mywishlist',[encrypt(Auth::user()->id)])}} @else {{ url('login') }} @endif" class="pushable pushable-contact">
-          <span class="shadow"></span>
-          <span class="edge"></span>
-           <span class="front front-menu">
-           Your Points
-           </span>
-</a>--> 
-<a href="{{route('website.yourpoints')}}" class="btn-blue treat-button">My points</a>
+    <a class="nav-link" href="{{route('website.yourpoints')}}">Your Points</a>
   </li>
 </ul> 
