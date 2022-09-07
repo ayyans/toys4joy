5c70a8ca899cefab0771a9beb249c218a1ebe371
mohamed ayman yeddes <ayman.yed123@gmail.com>
2022-09-07 12:41:28 +0300

    Update category_btn.blade.php

--- resources/views/website/layouts/category_btn.blade.php
+++ resources/views/website/layouts/category_btn.blade.php
@@ -1,6 +1,8 @@
-
 <div class="row for-mobile">
 <div class="col text-center">
-<a  class="btn btn-catmenu" data-bs-toggle="offcanvas" href="#offcanvasLeft-cats" role="button" aria-controls="offcanvasLeft-cats">Categories</a>
+<div class="treat-wrapper">
+    <a class="btn-red treat-button" data-bs-toggle="offcanvas" href="#offcanvasLeft-cats" role="button" aria-controls="offcanvasLeft-cats">Categories</a>
+    </div>
+<!--<a  class="btn btn-catmenu" data-bs-toggle="offcanvas" href="#offcanvasLeft-cats" role="button" aria-controls="offcanvasLeft-cats">Categories</a>-->
 </div>
 </div>
