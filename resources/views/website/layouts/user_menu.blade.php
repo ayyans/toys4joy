530053cafb853f7e93d1aaa89c95d65fe27d9e42
mohamed ayman yeddes <ayman.yed123@gmail.com>
2022-09-07 16:27:28 +0300

    Update user_menu.blade.php

--- resources/views/website/layouts/user_menu.blade.php
+++ resources/views/website/layouts/user_menu.blade.php
@@ -1,15 +1,15 @@
-<ul class="nav nav-pills nav-fill">
+<ul class="nav nav-pills nav-fill treat-wrapper">
   <li class="nav-item">
     <?php //{{ route('website.newarrivals') }} ?>
-    <a class="nav-link active" aria-current="page" href="{{ route('website.bestoffers') }}">Special Prices</a>
+    <a class="nav-link btn-red treat-button" aria-current="page" href="{{ route('website.bestoffers') }}">Special Prices</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="@if(Auth::check())  {{route('website.mywishlist',[encrypt(Auth::user()->id)])}} @else {{ url('login') }} @endif">Wish List</a>
+    <a class="nav-link btn-green treat-button" href="@if(Auth::check())  {{route('website.mywishlist',[encrypt(Auth::user()->id)])}} @else {{ url('login') }} @endif">Wish List</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="{{route('website.giftcard')}}">E-Gift Cards</a>
+    <a class="nav-link btn-yellow treat-button" href="{{route('website.giftcard')}}">E-Gift Cards</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="{{route('website.yourpoints')}}">Your Points</a>
+    <a class="nav-link btn-blue treat-button" href="{{route('website.yourpoints')}}">Your Points</a>
   </li>
 </ul> 
