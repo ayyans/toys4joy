e03680cc0eeeed459b6b4d53df925c09d0c76f68
mohamed ayman yeddes <ayman.yed123@gmail.com>
2022-09-22 04:13:11 +0300

    Update product_details_backup.blade.php

--- resources/views/website/product_details_backup.blade.php
+++ resources/views/website/product_details_backup.blade.php
@@ -1,59 +1,67 @@
 @extends('website.layouts.master')
 @section('content')
+<style>
+  .xzoom-source img,
+  .xzoom-preview img,
+  .xzoom-lens img {
+    display: block;
+    max-width: none;
+    max-height: none;
+    -webkit-transition: none;
+    -moz-transition: none;
+    -o-transition: none;
+    transition: none;
+  }
+</style>
 <main class="product-detail-page">
-<div class="container-fluid">
+  <div class="container-fluid">
     <div class="row">
-        <div class="col-3 categories-col">
-            <div class="d-flex flex-column flex-shrink-0" > 
-                <div class="for-mobile mbl-banner">
-                    <ul class="nav nav-pills nav-fill">
-                      <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Best Offers</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Wish List</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">E-Gift Cards</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link">Your Points</a>
-                      </li>
-                    </ul>
-                </div>
-                <div class="categories">
-                    <h2 class="for-desktop">Categories</h2>
-                    <h2 class="for-mobile">Select Category</h2>
-                    <ul class="nav nav-pills flex-column mb-auto menu">
-                    @include('website.layouts.category_menu')
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="col-9 pro-detail-col">
-        <div class="for-desktop">
+      <div class="col-3 categories-col">
+        <div class="d-flex flex-column flex-shrink-0">
+          <div class="for-mobile mbl-banner">
             @include('website.layouts.user_menu')
+          </div>
+          <div class="categories mb-3">
+                    <h2 class="for-desktop">Categories</h2>
+                    @include('website.layouts.category_btn')
+                    <ul class="nav nav-pills flex-column mb-auto menu for-desktop">
+                     @include('website.layouts.category_menu')
+                      
+                    </ul>
+          </div>
         </div>
-            <div class="d-flex products-detail">
-                <div class="product-gallery">
-                    <div class="images p-3">
-                        <div class="text-center p-4">
-                            <img id="main-image" class="xzoom" src="{{asset('products/'.$products->featured_img)}}" xoriginal="{{asset('products/'.$products->featured_img)}}"/>
-                        </div>
-                        <div class="thumbnail text-center xzoom-thumbs">
-                            @foreach($gallery as $photos)
-                            <img class="xzoom-gallery" onclick="change_image(this)" src="{{asset('products/'.$photos->gallery_img)}}" width="150" xpreview="{{asset('products/'.$photos->gallery_img)}}">
-                           @endforeach
-                        </div>
-                    </div>
-                </div>
-                <div class="product-detail">
-                    <div class="brand"><h5>Brand : {{$products->brand_name}}</h5></div>
-                    <div class="title"><h2>{{$products->title}}</h2></div>
-                    @if($products->logo)
-                    <div class="nerf-logo"><img src="{{asset('uploads/'.$products->logo)}}"/></div>
-                    @endif
-                    <div class="reviews">
+      </div>
+      <div class="col-9 pro-detail-col">
+        <div class="for-desktop">
+          @include('website.layouts.user_menu')
+        </div>
+        <div class="d-flex products-detail">
+          <div class="product-gallery">
+            <div class="images p-3">
+              <div class="text-center p-4">
+                <img id="main-image" class="" src="{{asset('products/'.$products->featured_img)}}"
+                  xoriginal="{{asset('products/'.$products->featured_img)}}" />
+              </div>
+              <div class="thumbnail text-center xzoom-thumbs">
+                @foreach($gallery as $photos)
+                <img class="xzoom-gallery" onclick="change_image(this)"
+                  src="{{asset('products/'.$photos->gallery_img)}}" width="150"
+                  xpreview="{{asset('products/'.$photos->gallery_img)}}">
+                @endforeach
+              </div>
+            </div>
+          </div>
+          <div class="product-detail">
+            <div class="brand">
+              <h5>Brand : {{$products->brand_name}}</h5>
+            </div>
+            <div class="title">
+              <h2>{{$products->title}}</h2>
+            </div>
+            @if($products->logo)
+            <div class="nerf-logo"><img src="{{asset('uploads/'.$products->logo)}}" /></div>
+            @endif
+            <!--<div class="reviews">
                         <div class="d-flex stars">
                             <img src="{{asset('website/img/star.png')}}"/>
                             <img src="{{asset('website/img/star.png')}}"/>
@@ -62,134 +70,178 @@
                             <img src="{{asset('website/img/star.png')}}"/>
                         </div>
                         <p>Be the first one to review this product</p>
-                    </div>
-                    @if($products->discount)
-                    <del class="price"> QAR {{$products->unit_price}}</del>
-                    <h2 class="price"> QAR {{$products->discount}}</h2>
+                    </div>-->
+            @if($products->discount)
+            <del class="price"> QR {{$products->unit_price}}</del>
+            <h2 class="price"> QR {{$products->discount}}</h2>
 
-                    @else
-                    <h2 class="price"> QAR {{$products->unit_price}}</h2>
-                    @endif
+            @else
+            <h2 class="price"> QR {{$products->unit_price}}</h2>
+            @endif
 
-                    <div class="earn-points"><h5>Earn Reward Points Worth {{$products->points}} Points<span>(Only for registered users)</span></h5></div>
-                    @if(!empty($products->unit))
-                    <div class="suitable-age">
-                        <img src="{{asset('website/img/boy.svg')}}"/> <span>Recommended Age: {{ $products->recommended_age !== null ? "{$products->formatRecommendedAge($products->recommended_age)} +" : 'All' }} </span>
-                    </div>
-                    @endif
-                    <p class="product-desc"><span>Description</span> : {!! $products->long_desc !!}</p>
-                    @if($products->qty!=0)
-                    <div class="d-flex icons-area">
-                       
-                        <form action="{{route('website.payasguest')}}" method="POST" id="productFRM">
-                            @csrf
-                            <input type="hidden" name="prod_id" value="{{$products->id}}" id="prod_id" />
-                            <input type="hidden" name="amt" value="{{$products->unit_price}}" id="amt" />
-                            <div class="qty block">
-                                <label for="quantity">Quantity</label>
-                                <input type="number" value="1" id="quantity" name="quantity" min="{{$products->min_qty}}" max="{{$products->qty}}">
-                            </div>
-                        </form>
-                       
-                        <!-- <div class="add-to-cart block">
+            <div class="earn-points">
+              <h5>Earn Reward Points Worth {{$products->rewardPoints}} Points<span>(Only for registered users)</span>
+              </h5>
+            </div>
+            @if(!empty($products->unit))
+            <div class="suitable-age">
+              <img src="{{asset('website/img/boy.svg')}}" /> <span>Recommended Age: {{ $products->recommended_age !==
+                null ? "{$products->formatRecommendedAge($products->recommended_age)} +" : 'All' }} </span>
+            </div>
+            @endif
+            <p class="product-desc"><span>Description</span> : {!! $products->long_desc !!}</p>
+            @if($products->qty!=0)
+            <div class="d-flex icons-area">
+
+              <form action="{{route('website.payasguest')}}" method="POST" id="productFRM">
+                @csrf
+                <input type="hidden" name="product_id" value="{{$products->id}}" id="prod_id" />
+                <div class="qty block">
+                  <label for="quantity">Quantity</label>
+                  <input type="number" value="1" id="quantity" name="quantity" min="{{$products->min_qty}}"
+                    max="{{$products->qty}}">
+                </div>
+              </form>
+
+              <!-- <div class="add-to-cart block">
                             <label>Add to Cart</label>
                             <div class="icon" id="addtocart"><img src="{{asset('website/img/cart-icon.png')}}"/></div>
                         </div> -->
-                        <div class="like block">
+              <!--<div class="like block">
                             <label>Like</label>
                             <div class="icon"><img src="{{asset('website/img/thumb-up.svg')}}"/></div>
-                        </div>
-                        @if(Auth::check())
-                        <div class="wishlist block">
-                            <label>Wish List</label>
-                            <div class="icon" id="addwishlist"><img src="{{asset('website/img/bi_heart.svg')}}"/></div>
-                        </div>
-                        @else
-                        <a href="{{ url('login') }}">
-                        <div class="wishlist block">
-                            <label>Wish List</label>
-                            <div class="icon"><img src="{{asset('website/img/bi_heart.svg')}}"/></div>
-                        </div>
-                        </a>
-                        @endif
-                    </div>
-                    <div class="d-flex btn-area">
-                        <div class="guest"><a href="javascript:void(0)" id="checkoutaddtocart"><span>Checkout</span></a></div>
-                        <div class="member"><a href="javascript:void(0)" id="addtocart"><span>Add to Cart</span></a></div>
-                    </div>
-                    @else
-                    <p style="color:red"><strong>Out of stock</strong></p>
-                    @endif
+                        </div>-->
+              @if(Auth::check())
+              <div class="wishlist block">
+                <label>Wish List</label>
+                <div class="icon" id="addwishlist"><img src="{{asset('website/img/bi_heart.svg')}}" /></div>
+              </div>
+              @else
+              <a href="{{ url('login') }}">
+                <div class="wishlist block">
+                  <label>Wish List</label>
+                  <div class="icon"><img src="{{asset('website/img/bi_heart.svg')}}" /></div>
                 </div>
+              </a>
+              @endif
             </div>
+            <div class="d-flex btn-area">
+              <div class="guest"><a href="javascript:void(0)" id="checkoutaddtocart"><span>Checkout</span></a></div>
+              <div class="member"><a href="javascript:void(0)" id="addtocart"><span>Add to Cart</span></a></div>
+            </div>
+            @else
+            <p style="color:red"><strong>Out of stock</strong></p>
+            @endif
+          </div>
         </div>
+      </div>
     </div>
-</div>
- 
+  </div>
+
 </main>
 @stop
 
 @push('otherscript')
 <script>
-    $(function(){
-        $("#addtocart").click(function(){        
-        var form =$("form#productFRM")[0]; 
-        var form2 =new FormData(form);
-        $("#cover-spin").show();
-        $.ajax({
-            url:"{{route('website.addTocart')}}",
-            type:"POST",
-            data:form2,
-            cache:false,
-            contentType:false,
-            processData:false,
-            success:function(res){
-                $("#cover-spin").hide();
-                var js_data = JSON.parse(JSON.stringify(res));
-                if(js_data.status==200){
-                    toastr.success('Product added to cart');
-                    location.reload();
-                }else if(js_data.msg=='3'){
-                    toastr.error('product out of stock! please check product stock');
-                    return false; 
-                }
-                else{
-                    toastr.error('something went wrong');
-                    return false; 
-                }
-            }
-        })
-    })
-})
+  $(function() {
+    $("#addtocart").click(function() {
+      const url = "{{route('website.addTocart')}}";
+      const form = $('form#productFRM')[0];
+      const data = new FormData(form);
+      $("#cover-spin").show();
+      fetch(url, {
+        method: 'POST',
+        body: data
+      })
+      .then(res => res.json())
+      .then(res => {
+        $("#cover-spin").hide();
+        if (res.status) {
+            toastr.success(res.message);
+            headercart();
+        } else {
+            toastr.error(res.message)
+        }
+      })
+      .catch(err => console.log(err));
+
+        // var form =$("form#productFRM")[0]; 
+        // var form2 =new FormData(form);
+        // console.log(form2);
+        // $("#cover-spin").show();
+        // $.ajax({
+        //     url:"{{route('website.addTocart')}}",
+        //     type:"POST",
+        //     data:form2,
+        //     cache:false,
+        //     contentType:false,
+        //     processData:false,
+        //     success:function(res){
+        //         $("#cover-spin").hide();
+        //         var js_data = JSON.parse(JSON.stringify(res));
+        //         if(js_data.status==200){
+        //             toastr.success('Product added to cart');
+        //             location.reload();
+        //         }else if(js_data.msg=='3'){
+        //             toastr.error('product out of stock! please check product stock');
+        //             return false; 
+        //         }
+        //         else{
+        //             toastr.error('something went wrong');
+        //             return false; 
+        //         }
+        //     }
+        // })
+    });
+  });
 </script>
 
 <script>
-    $(function(){
-        $("a#checkoutaddtocart").click(function(e){
-            e.preventDefault();
+  $(function(){
+    $("a#checkoutaddtocart").on('click', function(e) {
+      e.preventDefault();
 
-            var form =$("form#productFRM")[0]; 
-            var form2 =new FormData(form);
-            $("#cover-spin").show();
-            $.ajax({
-                url:"{{route('website.addTocart')}}",
-                type:"POST",
-                data:form2,
-                cache:false,
-                contentType:false,
-                processData:false,
-                success:function(res){
-                    window.location.href="{{route('website.cartpage')}}";
-                }
-            })
-        })
+      const url = "{{route('website.addTocart')}}";
+      const form = $('form#productFRM')[0];
+      const data = new FormData(form);
+      $("#cover-spin").show();
+      fetch(url, {
+        method: 'POST',
+        body: data
+      })
+      .then(res => res.json())
+      .then(res => {
+        $("#cover-spin").hide();
+        if (res.status) {
+          window.location.href="{{route('website.cartpage')}}";
+        } else {
+          toastr.error(res.message)
+        }
+      })
+      .catch(err => console.log(err));
+
+            // var form =$("form#productFRM")[0]; 
+            // var form2 =new FormData(form);
+            // $("#cover-spin").show();
+            // $.ajax({
+            //     url:"{{route('website.addTocart')}}",
+            //     type:"POST",
+            //     data:form2,
+            //     cache:false,
+            //     contentType:false,
+            //     processData:false,
+            //     success:function(res){
+            //         window.location.href="{{route('website.cartpage')}}";
+            //     }
+            // })
     })
+  })
 </script>
 
 <!-- pay as wishlist -->
 
-<script>
-    $("#payasmember").click(function(e){
+{{-- <script>
+  $("#payasmember").click(function(e){
         e.preventDefault();
         var form = $("form#productFRM")[0];
         var form2 = new FormData(form);       
@@ -217,10 +269,10 @@
             }
         })
     })
-</script>
+</script> --}}
 
 <script>
-    $("#addwishlist").click(function(){
+  $("#addwishlist").click(function(){
     var form =$("form#productFRM")[0]; 
     var form2 =new FormData(form);
     $("#cover-spin").show();
@@ -250,14 +302,14 @@
 })
 </script>
 
-<script id="rendered-js" >
-(function ($) {
+<script id="rendered-js">
+  (function ($) {
   $(document).ready(function () {
-    $('.xzoom, .xzoom-gallery').xzoom({ zoomWidth: 400, title: true, tint: '#333', Xoffset: 15 });
-    $('.xzoom2, .xzoom-gallery2').xzoom({ position: '#xzoom2-id', tint: '#ffa200' });
+    $('.xzoom, .xzoom-gallery').xzoom({  title: true, tint: '#333', Xoffset: 15, lensShape: "circle",defaultScale:-1 });
+    /*$('.xzoom2, .xzoom-gallery2').xzoom({ position: '#xzoom2-id', tint: '#ffa200' });
     $('.xzoom3, .xzoom-gallery3').xzoom({ position: 'lens', lensShape: 'circle', sourceClass: 'xzoom-hidden' });
     $('.xzoom4, .xzoom-gallery4').xzoom({ tint: '#006699', Xoffset: 15 });
-    $('.xzoom5, .xzoom-gallery5').xzoom({ tint: '#006699', Xoffset: 15 });
+    $('.xzoom5, .xzoom-gallery5').xzoom({ tint: '#006699', Xoffset: 15 });*/
 
     //Integration with hammer.js
     var isTouchSupported = ('ontouchstart' in window);
@@ -356,7 +408,7 @@
               event.gesture.preventDefault();
             });
           };
-
+          
           function openmagnific() {
             if (counter == 2) {
               xzoom.closezoom();
@@ -402,6 +454,6 @@
   });
 })(jQuery);
 //# sourceURL=pen.js
-</script> 
+</script>
 
 @endpush
