de3e68d5aa24a779b5544bf006117f633aba9bc6
mohamed ayman yeddes <ayman.yed123@gmail.com>
2022-10-02 03:33:26 +0300

    Update category_btn.blade.php

--- resources/views/website/layouts/category_btn.blade.php
+++ resources/views/website/layouts/category_btn.blade.php
@@ -1,7 +1,7 @@
 <div class="row for-mobile">
 <div class="col text-center">
 <div class="treat-wrapper">
-    <a class="btn-red treat-button" data-bs-toggle="offcanvas" href="#offcanvasLeft-cats" role="button" aria-controls="offcanvasLeft-cats">Categories</a>
+    <a class="btn-red treat-button" data-bs-toggle="offcanvas" href="#offcanvasLeft-cats" role="button" aria-controls="offcanvasLeft-cats">{{__('trans.Categories')}}</a>
     </div>
 <!--<a  class="btn btn-catmenu" data-bs-toggle="offcanvas" href="#offcanvasLeft-cats" role="button" aria-controls="offcanvasLeft-cats">Categories</a>-->
 </div>
