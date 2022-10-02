929d0ee1b23d69e186d25615297aaec452093dd5
mohamed ayman yeddes <ayman.yed123@gmail.com>
2022-10-03 00:20:45 +0300

    Update verifyotp.blade.php

--- resources/views/website/verifyotp.blade.php
+++ resources/views/website/verifyotp.blade.php
@@ -6,30 +6,17 @@
     <div class="row">
         <div class="col-3 categories-col">
             <div class="d-flex flex-column flex-shrink-0" > 
-                <div class="for-mobile mbl-banner">
-                    <ul class="nav nav-pills nav-fill">
-                      <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Best Offers</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Wish List</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">E-Gift Cards</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link">Your Points</a>
-                      </li>
+            <div class="for-mobile mbl-banner">
+            @include('website.layouts.user_menu')
+          </div>
+          <div class="categories mb-3">
+                    <h2 class="for-desktop">{{__('trans.Categories')}}</h2>
+                    @include('website.layouts.category_btn')
+                    <ul class="nav nav-pills flex-column mb-auto menu for-desktop">
+                     @include('website.layouts.category_menu')
+                      
                     </ul>
-                </div>
-                <div class="categories">
-                    <h2 class="for-desktop">Categories</h2>
-                    <h2 class="for-mobile">Select Category</h2>
-                    <ul class="nav nav-pills flex-column mb-auto menu">
-                    @include('website.layouts.category_menu')
-                                 
-                    </ul>
-                </div>
+          </div>
             </div>
         </div>
     	<div class="col-9 form-col">
@@ -38,11 +25,11 @@
             </div>
             
             <div class="form-group">
-              <p class="text-center prompt">Please verify the OTP code you received by SMS<span>*</span></p>
+              <p class="text-center prompt">{{__('trans.Please verify the OTP code you received by SMS')}}<span>*</span></p>
                 <form method="POST" action="{{ url('confermotp') }}" id="confermotpform" class="d-flex digit-group" data-group-name="digits" data-autosubmit="false" autocomplete="off">
                     <input type="text" autofocus id="digit-1" name="otp" data-next="digit-2" />
                 </form>
-                <button onclick="submitform()" class="vertical-shake">Submit</button>
+                <button onclick="submitform()" class="vertical-shake">{{__('trans.Submit')}}</button>
             </div>
         
     </div>
