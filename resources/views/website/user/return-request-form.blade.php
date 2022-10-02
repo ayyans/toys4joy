08092eb370bf25c75cc05f1f93af745751e988b9
mohamed ayman yeddes <ayman.yed123@gmail.com>
2022-10-02 03:26:32 +0300

    Update return-request-form.blade.php

--- resources/views/website/user/return-request-form.blade.php
+++ resources/views/website/user/return-request-form.blade.php
@@ -6,33 +6,33 @@
       <div class="row">
         <div class="col-6">
           <div class="want-to-return">
-            <h3>Why do you want to return this item?</h3>
+            <h3>{{__('trans.Why do you want to return this item?')}}</h3>
             <div class="form-check">
               <input class="form-check-input" type="radio" name="reason" id="first_option" value="The item isn't working properly." checked>
               <label class="form-check-label" for="first_option">
-                The item isn't working properly.
+              {{__("trans.The item isn't working properly.")}}
               </label>
             </div>
             <div class="form-check">
               <input class="form-check-input" type="radio" name="reason" id="second_option" value="I received wrong item.">
               <label class="form-check-label" for="second_option">
-                I received wrong item.
+              {{__('trans.I received wrong item.')}}
               </label>
             </div>
             <div class="form-check">
               <input class="form-check-input" type="radio" name="reason" id="third_option" value="Product was damaged.">
               <label class="form-check-label" for="third_option">
-                Product was damaged.
+              {{__('trans.Product was damaged.')}}
               </label>
             </div>
             <div class="form-check">
               <input class="form-check-input" type="radio" name="reason" id="fourth_option" value="Part of the product is missing.">
               <label class="form-check-label" for="fourth_option">
-                Part of the product is missing.
+              {{__('trans.Part of the product is missing.')}}
               </label>
             </div>
             <div class="form-group message">
-              <label>Tell Us More</label>
+              <label>{{__('trans.Tell Us More')}}</label>
               <textarea name="detail" required></textarea>
             </div>
           </div>
@@ -43,8 +43,8 @@
               <input type="file" id="receipt" name="receipt">
               <label for="receipt"><img src="{{ asset('website/img/attach.png') }}" /></label>
             </div>
-            <label>Attach copy of your receipt</label>
-            <button type="submit" class="vertical-shake">Submit</button>
+            <label>{{__('trans.Attach copy of your receipt')}}</label>
+            <button type="submit" class="vertical-shake">{{__('trans.Submit')}}</button>
           </div>
         </div>
       </div>
