418ad864f46aa5ad6632427587e47582f90b4297
mohamed ayman yeddes <ayman.yed123@gmail.com>
2022-10-03 00:19:00 +0300

    Update guestthanksgiftcard.blade.php

--- resources/views/website/guestthanksgiftcard.blade.php
+++ resources/views/website/guestthanksgiftcard.blade.php
@@ -7,9 +7,9 @@
         <div class="col-3"></div>
     	<div class="col-6">
             <div class="text-center thanks-box">
-                <h3>Thanks for Purchasing Gift Card</h3>
+                <h3>{{__('trans.Thanks for Purchasing Gift Card')}}</h3>
                 <img src="{{asset('website/img/logo-t4j.png')}}">
-                <p>One of our team member will contact you shortly to get your location & necessary information</p>
+                <p>{{__('trans.One of our team member will contact you shortly to get your location & necessary information')}}</p>
                 <div class="text-center d-flex print">
                     <img src="{{asset('website/img/printer.png')}}">
                     <h4><a href="{{ url('generateinvoicegiftcard') }}/{{ $order_number }}">Print my Receipt</a></h4>
