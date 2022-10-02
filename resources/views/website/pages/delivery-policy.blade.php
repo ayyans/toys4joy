a75305bed5bc0e3ff1587ba22c589319eb8faadb
mohamed ayman yeddes <ayman.yed123@gmail.com>
2022-10-02 03:30:17 +0300

    Update delivery-policy.blade.php

--- resources/views/website/pages/delivery-policy.blade.php
+++ resources/views/website/pages/delivery-policy.blade.php
@@ -10,7 +10,7 @@
                     <!--<img src="{{asset('website/img/t1.png')}}" class="img-fluid">-->
                 </div>
                 <div class="categories mb-3">
-                    <h2 class="for-desktop">Categories</h2>
+                    <h2 class="for-desktop">{{__('trans.Categories')}}</h2>
                     @include('website.layouts.category_btn')
                     <ul class="nav nav-pills flex-column mb-auto menu for-desktop">
                      @include('website.layouts.category_menu')
@@ -25,16 +25,16 @@
             </div>
             <div class="t-and-c-text">
                 <div class="content">
-                    <h4>Delivery Policy</h4>
-                    <p>Toys 4 Joy is the only company that make free delivery without any limit amount of purchasing, we're committed to making our delivery process as quick and pleasant as possible, because we know how important it is that your goods arrive on time and in perfect condition.</p>
-                    <p>To provide the utmost satisfaction, majority of deliveries are carried by our own fleet and Toys 4 Joy personnel.</p>
-                    <p>Free delivery for each order </p>
+                    <h4>{{__('trans.Delivery Policy')}}</h4>
+                    <p>{{__("policies.Toys 4 Joy is the only company that make free delivery without any limit amount of purchasing, we're committed to making our delivery process as quick and pleasant as possible, because we know how important it is that your goods arrive on time and in perfect condition.")}}</p>
+                    <p>{{__('policies.To provide the utmost satisfaction, majority of deliveries are carried by our own fleet and Toys 4 Joy personnel.')}}</p>
+                    <p>{{__('policies.Free delivery for each order')}} </p>
                     <ul>
-                        <li>Free delivery for all orders above 99 QR within 3 hours.</li>
-                        <li>Free delivery for all orders below 99 QR within 24 hours.</li>
-                        <li>Free delivery and assembling on products that includes assembly requirement within 24 to 48 hours.</li>
+                        <li>{{__('policies.Free delivery for all orders above 99 QR within 3 hours.')}}</li>
+                        <li>{{__('policies.Free delivery for all orders below 99 QR within 24 hours.')}}</li>
+                        <li>{{__('policies.Free delivery and assembling on products that includes assembly requirement within 24 to 48 hours.')}}</li>
                     </ul>
-                    <p><strong>Note: All operation in Toys 4 Joy closed before, during, and after one hour of Friday Pray.</strong></p> 
+                    <p><strong>{{__('policies.Note: All operation in Toys 4 Joy closed before, during, and after one hour of Friday Pray.')}}</strong></p> 
 
                 </div>
             </div>
