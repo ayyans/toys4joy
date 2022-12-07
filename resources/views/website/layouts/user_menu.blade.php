a90efc232cfba517fa0f9665bd0835ab74fc46be
Talha Moaz Sarwar <iamtms@hotmail.com>
2022-12-07 18:10:22 +0500

    sale upto 60% added

--- resources/views/website/layouts/user_menu.blade.php
+++ resources/views/website/layouts/user_menu.blade.php
@@ -1,7 +1,7 @@
 <ul class="nav nav-pills nav-fill treat-wrapper" style="padding-right: 0;">
   <li class="nav-item">
     <?php //{{ route('website.newarrivals') }} ?>
-    <a class="nav-link btn-red treat-button" aria-current="page" href="{{ route('website.bestoffers') }}">{{ __('Special Prices') }}</a>
+    <a class="nav-link btn-red treat-button" aria-current="page" href="{{ route('website.bestoffers') }}">{{ __('Sale up to 60%') }}</a>
   </li>
   <li class="nav-item">
     <a class="nav-link btn-green treat-button" href="@if(Auth::check())  {{route('website.mywishlist',[encrypt(Auth::user()->id)])}} @else {{ url('login') }} @endif">{{ __('Wish List') }}</a>
